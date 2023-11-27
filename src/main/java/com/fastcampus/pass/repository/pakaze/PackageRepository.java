900c8c8fe49defc8119622171d52c16ffbb2ea54
jonnygim <jonnygim.dev@gmail.com>
2023-11-27 17:41:53 +0900

    entity 추가

---
+++ src/main/java/com/fastcampus/pass/repository/pakaze/PackageRepository.java
@@ -0,0 +1,11 @@
+package com.fastcampus.pass.repository.pakaze;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+    List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable pageable);
+}
